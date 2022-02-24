80d93090806d547de3d675ed32f8ed30b4fcaa99
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-24 13:24:27 +0300

    Kimlik Doğrulama Başarılı Cevap (Spring)

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -8,6 +8,10 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.hoaxify.ws.shared.Views;
+
 import lombok.Data;
 
 @Data
@@ -21,15 +25,21 @@ public class User {
 	@NotNull(message = "{hoaxify.constraints.username.NotNull.message}")
 	@Size(min = 4,max=255)
 	@UniqueUsername(message ="{hoaxify.constraints.username.UniqueUsername.message}" )
+	@JsonView(Views.Base.class)
 	private String username;
 	
 	@NotNull
 	@Size(min = 4,max=255)
+	@JsonView(Views.Base.class)
 	private String displayName;
 	
 	@NotNull
 	@Size(min = 4,max=255)
 	//@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]$")
 	private String password;
+	
+	@JsonView(Views.Base.class)
+	private String image;
+	
 
 }
